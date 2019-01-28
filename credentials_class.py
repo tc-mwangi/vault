0f58dd98254c5cbcc4a22452cd1fb4deb9f2e7fd
tc-mwangi <wanjirumwangi.learn@gmail.com>
2019-01-28 14:35:23 +0300

    add verification method to Credentials

--- credentials_class.py
+++ credentials_class.py
@@ -1,15 +1,21 @@
+from user_class import User
+
 class Credentials:
     
     """
-    Class that creates details for online accounts and generates passwords/allows user passwords.
+    Class that creates details for online accounts, saves details and generates passwords/allows user passwords.
     """
 
     credentials_list = [] #
     user_credentials_list = [] #
 
     @classmethod
-    def find_by_user_name(cls, user_name, user_password):
-
+    def check_user(cls, user_name, user_password):
         '''
-        method that checks whether user has a vault account i.e is in the user_list.
+        method checks whether user is registered/is in the user_list.
         '''
+        this_user = "" #retrieve from user input
+        for user in User.users_list:
+            if(user.user_name == user_name and user_password == user_password):
+                this_user = user.first_name
+        return this_user
--- run_vault.py
+++ run_vault.py
@@ -21,23 +21,26 @@ def save_user(user):
 	User.save_user(user)
 
 # verify user before they can proceed to create credentials.
-def check_user(first_name,password):
+def check_user(user_name,user_password):
 	'''
 	function checks to see if a user is registered in the user list.
 	'''
-	checking_user = Credentials.check_user(first_name,password)
+	checking_user = Credentials.check_user(user_name,user_password)
 	return checking_user
 
 def main():
-    
+    print("\n")
     print("Welcome to Vault, your reliable account/password manager.")
+    print("\n")
     while True:
         print("What would you like to do today?" )
-        print(" li - log-in, ca - create vault account, ex - exit vault")
-        short_code = input("enter short code").lower()
+        print("\n")
+        print(" [cv] - create vault account, [li] - log-in,  [ex] - exit vault")
+        print("\n")
+        short_code = input("Enter short code: ").lower()
 
-        if short_code == "ca":
-            #ask for login details
+        if short_code == "cv":
+            #ask for details
             print("Enter the following details:")
             print ("First name ....")
             first_name = input()
@@ -47,8 +50,69 @@ def main():
             user_name = input()
             print ("Password ....")
             user_password = input()
+
+            #save new user
+            save_user(create_user(first_name, last_name, user_name, user_password))
+            print(f'New vault Account Details: {first_name} {last_name} {user_name} using password: "{user_password}"')
+
+        elif short_code == "li":
+            print ("Username ....")
+            user_name = input()
+            print ("Password ....")
+            user_password = input()
+
+            #verify user
+            check_user
+
+            # user_exists = verify_user(user_name,password)
+			# if user_exists == user_name:
+			# 	print(" ")
+			# 	print(f'Welcome {user_name}. Please choose an option to continue.')
+			# 	print(' ')
+			# 	while True:
+			# 		print("-"*60)
+			# 		print('Navigation codes: \n cc-Create a Credential \n dc-Display Credentials \n copy-Copy Password \n ex-Exit')
+			# 		short_code = input('Enter a choice: ').lower().strip()
+			# 		print("-"*60)
+			# 		if short_code == 'ex':
+			# 			print(" ")
+			# 			print(f'Goodbye {user_name}')
+			# 			break
+			# 		elif short_code == 'cc':
+			# 			print(' ')
+			# 			print('Enter your credential details:')
+			# 			site_name = input('Enter the site\'s name- ').strip()
+			# 			account_name = input('Enter your account\'s name - ').strip()
+
+        
             
 
+        elif short_code == "ex":
+            print("Good bye!")
+            break
+        else:
+            print("Please use the short codes provided !")
+
+        
+
+        
+
+            
+           
+			
+
+
+
+        # elif short_code == "li":
+        #     #ask for login details
+        #     print("Vault Login")
+        #     print("\n")
+        #     print ("Username ....")
+        #     user_name = input()
+        #     print ("Password ....")
+        #     user_password = input()
+            
+            
 if __name__ == '__main__':
 
     main()
